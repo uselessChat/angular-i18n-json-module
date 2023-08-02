3e32ffebc22f6e50ede9652a3b6eac71a3367ef6
uselessChat <uselessChat@github.com>
2023-08-02 14:05:50 -0600

    Add type I18nRecord

--- src/app/i18n/i18n-loader.ts
+++ src/app/i18n/i18n-loader.ts
@@ -2,8 +2,10 @@ import { TranslateLoader } from '@ngx-translate/core';
 import { Observable, of } from 'rxjs';
 import I18nEN from 'src/assets/i18n/en.json';
 
+export type I18nRecord = Record<string, string | any>;
+
 export class I18nLoader implements TranslateLoader {
-  getTranslation(lang: string): Observable<any> {
+  getTranslation(lang: string): Observable<I18nRecord> {
     switch (lang) {
       case 'en': return of(I18nEN);
     }
